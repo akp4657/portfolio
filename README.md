0dde3c7b622f91885020624d1f58d7913acaf465
Spine <igniteboost.net@gmail.com>
2024-09-24 15:32:25 -0400

    Updates for production

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal Blog and Portfolio
+
+Built using the Astro framework and Fuwari. A website for my personal projects, portfolio, and tech blog posts!
+
+Check it out [here](https://www.anthonypichardo.dev/)
--- src/config.ts
+++ src/config.ts
@@ -9,15 +9,15 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Anthony Pichardo',
-  subtitle: '',
+  subtitle: 'Web Developer from Rochester, NY',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 230,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 360,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: '',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false,         // Display the credit text of the banner image
